d7c99d8961bf4a1cd78834804a60f16d8180a956
MikenTNT <miguel.sanchezgon@gmail.com>
2019-12-02 20:58:54 +0100

    Added error numbers

--- cliente.h
+++ cliente.h
@@ -8,8 +8,15 @@
 #define RETRIES 5  /* number of times to retry before givin up */
 #define TIMEOUT 6
 
+/* Error numbers */
+#define ERR_NICKNAME 433
+#define ERR_ALREADYREGISTRED 462
+#define ERR_NOSUCHNICK 401
+#define ERR_NOSUCHCHANNEL 403
+
 
 void handler();
+
 void nick(char * nickName);
 void user(char * username);
 void mensajes(char * receptor, char * mensaje);
